68f8922066e9a59233d30f7177cda600fbff6c4e
drivelikejehu <pollyfactor@hotmail.com>
2021-02-15 19:57:43 -0500

    fluid image

--- src/examples/Images.js
+++ src/examples/Images.js
@@ -7,15 +7,15 @@ const getImages = graphql`
   {
     fixed: file(relativePath: { eq: "img3.jpeg" }) {
       childImageSharp {
-        fixed(width: 300, height: 400) {
+        fixed(width: 200, grayscale: true) {
           ...GatsbyImageSharpFixed
         }
       }
     }
-    fluid: file(relativePath: { eq: "img4.jpeg" }) { 
+    fluid: file(relativePath: { eq: "img4.jpeg" }) {
       childImageSharp {
         fluid {
-          src
+          ...GatsbyImageSharpFluid_withWebp_tracedSVG
         }
       }
     }
@@ -29,16 +29,16 @@ const Images = () => {
   return (
     <section className="images">
       <article className="single-image">
-        <h3>
-          <img src={img} width="100%"/>
-        </h3>
+        <h3>basic image</h3>
+        <img src={img} width="100%" />
       </article>
       <article className="single-image">
         <h3>fixed image/blur</h3>
-        <Image fixed={data.fixed.childImageSharp.fixed}/>
+        <Image fixed={data.fixed.childImageSharp.fixed} />
       </article>
       <article className="single-image">
         <h3>fluid image/svg</h3>
+        <Image fluid={data.fluid.childImageSharp.fluid} />
       </article>
     </section>
   )
